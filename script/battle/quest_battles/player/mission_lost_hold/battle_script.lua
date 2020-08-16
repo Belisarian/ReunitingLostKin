fac762022303b6e6322684140602e018ed0a4ba4
Zoidberg <MikeVestergaard@msn.com>
2020-08-16 20:08:42 +0200

    16.08.20 version
    Zoids

--- script/battle/quest_battles/player/mission_lost_hold/battle_script.lua
+++ script/battle/quest_battles/player/mission_lost_hold/battle_script.lua
@@ -37,15 +37,31 @@ gb:set_cutscene_during_deployment(true);
 
 ------------------------------------------PLAYER---------------------------------------------------
 ga_player = gb:get_army(gb:get_player_alliance_num(), 1, ""); -- attacker
+
+
 ------------------------------------------ALLY---------------------------------------------------
+------------------------------------------DEPLOYED-----------------------------------------------
 ga_battle_lost_hold_character_name_dwf_ally = gb:get_army(gb:get_player_alliance_num(),"battle_lost_hold_character_name_dwf_ally"); -- #0 Char + units
-ga_battle_lost_hold_character_name_dwf_ally_1 = gb:get_army(gb:get_player_alliance_num(),"battle_lost_hold_character_name_dwf_ally_1"); -- #1 Char + units
---ga_battle_lost_hold_character_name_dwf_ally_2 = gb:get_army(gb:get_player_alliance_num(),"battle_lost_hold_character_name_dwf_ally_2"); -- #2 Char + units
+
+
+------------------------------------------ALLY---------------------------------------------------
+------------------------------------------REINFORCEMENT------------------------------------------
+ga_battle_lost_hold_character_name_dwf_ally_1 = gb:get_army(gb:get_player_alliance_num(),"battle_lost_hold_character_name_dwf_ally_1"); -- #1 Char "King"
+ga_battle_lost_hold_army_dwf_ally_1_flee1 = gb:get_army(gb:get_player_alliance_num(),"battle_lost_hold_army_dwf_ally_1_flee1"); -- 1
+ga_battle_lost_hold_army_dwf_ally_1_flee2 = gb:get_army(gb:get_player_alliance_num(),"battle_lost_hold_army_dwf_ally_1_flee2"); -- 2
+ga_battle_lost_hold_army_dwf_ally_1_flee3 = gb:get_army(gb:get_player_alliance_num(),"battle_lost_hold_army_dwf_ally_1_flee3"); -- 3
+ga_battle_lost_hold_army_dwf_ally_1_flee4 = gb:get_army(gb:get_player_alliance_num(),"battle_lost_hold_army_dwf_ally_1_flee4"); -- 4
+ga_battle_lost_hold_army_dwf_ally_1_flee5 = gb:get_army(gb:get_player_alliance_num(),"battle_lost_hold_army_dwf_ally_1_flee5"); -- 5
 
  ------------------------------------------ENEMY---------------------------------------------------
+ ------------------------------------------DEPLOYED-----------------------------------------------
 ga_battle_lost_hold_character_name_nor = gb:get_army(gb:get_non_player_alliance_num(),"battle_lost_hold_character_name_nor"); -- #0 Char + units
+
+
+ ------------------------------------------ENEMY---------------------------------------------------
+ ------------------------------------------REINFORCEMENT------------------------------------------
 ga_battle_lost_hold_character_name_nor_1 = gb:get_army(gb:get_non_player_alliance_num(),"battle_lost_hold_character_name_nor_1"); -- #1 Char + units
---ga_battle_lost_hold_character_name_nor_2 = gb:get_army(gb:get_non_player_alliance_num(),"battle_lost_hold_character_name_nor_2"); -- #2 Char + units
+
 
 -------------------------------------------------------------------------------------------------
 ------------------------------------------ OBJECTIVES -------------------------------------------
@@ -54,11 +70,15 @@ ga_battle_lost_hold_character_name_nor_1 = gb:get_army(gb:get_non_player_allianc
 -------------------------------------------------------------------------------------------------
 --------------------------------- TIME KEEPER FROM BATTLE START --------------------------------
 -------------------------------------------------------------------------------------------------
-
+gb:message_on_time_offset("Wait_10",10000);
 gb:message_on_time_offset("Wait_1min",60000);
+gb:message_on_time_offset("Wait_1min+",80000);
 gb:message_on_time_offset("Wait_2min",120000);
+gb:message_on_time_offset("Wait_2min+",14000);
 gb:message_on_time_offset("Wait_3min",180000);
+gb:message_on_time_offset("Wait_3min+",200000);
 gb:message_on_time_offset("Wait_4min",240000);
+gb:message_on_time_offset("Wait_4min+",260000);
 gb:message_on_time_offset("Wait_5min",300000);
 gb:message_on_time_offset("Wait_6min",360000);
 gb:message_on_time_offset("Wait_7min",420000);
@@ -85,14 +105,32 @@ gb:message_on_time_offset("Wait_20min",1200000);
 ---------------------------------------ALLY DEFENDS-----------------------------------------------
 --------------------------------------------------------------------------------------------------
 --ga_battle_lost_hold_character_name_dwf_ally:defend_on_message("battle_started", 251, -132, 50);
+
+
 -------------------------------------------------------------------------------------------------
 -------------------------------------- REINFORCEMENT --------------------------------------------
 -------------------------------------------------------------------------------------------------
-ga_battle_lost_hold_character_name_nor_1:reinforce_on_message("Wait_1min", 10000);
---ga_battle_lost_hold_character_name_nor_2:deploy_at_random_intervals_on_message("Wait_3min",1,2,60000,60000); 
 
-ga_battle_lost_hold_character_name_dwf_ally_1:reinforce_on_message("Wait_1min", 10000);
---ga_battle_lost_hold_character_name_dwf_ally_2:deploy_at_random_intervals_on_message("Wait_3min",1,2,60000,60000); 
+
+ga_battle_lost_hold_character_name_nor_1:reinforce_on_message("Wait_10", 1000); -- Ememy enters
+
+ga_battle_lost_hold_character_name_dwf_ally_1:reinforce_on_message("Wait_10", 1000); -- King enters
+
+ga_battle_lost_hold_army_dwf_ally_1_flee1:reinforce_on_message("Wait_1min", 1000); -- Flee #1
+ga_battle_lost_hold_army_dwf_ally_1_flee1:rout_over_time_on_message("Wait_1min+",5000);  --  Flee #1 flees
+
+ga_battle_lost_hold_army_dwf_ally_1_flee2:reinforce_on_message("Wait_2min", 1000); -- Flee #2
+ga_battle_lost_hold_army_dwf_ally_1_flee2:rout_over_time_on_message("Wait_2min+",5000);  --  Flee #2 flees
+
+
+ga_battle_lost_hold_army_dwf_ally_1_flee3:reinforce_on_message("Wait_4min", 1000); -- Flee #3
+ga_battle_lost_hold_army_dwf_ally_1_flee3:rout_over_time_on_message("Wait_4min+",5000);  --  Flee #3 flees
+
+
+ga_battle_lost_hold_army_dwf_ally_1_flee4:reinforce_on_message("Wait_6min", 1000); -- Flee #4
+
+ga_battle_lost_hold_army_dwf_ally_1_flee5:reinforce_on_message("Wait_10min", 1000); -- Flee #5
+
 
 -------------------------------------------------------------------------------------------------
 --------------------------------------------- HINTS/MESSAGES ---------------------------------------------
