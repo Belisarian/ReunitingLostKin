5d0fa87703e4ae393a868e7996c7775ba2c31968
Tibor Ilić <tilic5@gmail.com>
2021-06-21 18:20:01 +0200

    also kill the moulder starting agent

--- script/campaign/mod/pj_campaign_edits.lua
+++ script/campaign/mod/pj_campaign_edits.lua
@@ -12,9 +12,24 @@ cm:add_first_tick_callback_new(function()
 		cm:convert_force_to_type(char:military_force(), "HORDE")
 	end, 7)
 
+	local function binding_iter(binding)
+		local pos = 0
+		local num_items = binding:num_items()
+		return function()
+				if pos < num_items then
+						local item = binding:item_at(pos)
+						pos = pos + 1
+						return item
+				end
+				return
+		end
+	end
+
 	-- kill Throt
 	local moulder_faction = cm:get_faction("wh2_main_skv_clan_moulder")
 	if moulder_faction and not moulder_faction:is_null_interface() then
-		cm:kill_character(moulder_faction:faction_leader():cqi(), true)
+		for char in binding_iter(moulder_faction:character_list()) do
+			cm:kill_character(char:cqi(), true)
+		end
 	end
 end)
