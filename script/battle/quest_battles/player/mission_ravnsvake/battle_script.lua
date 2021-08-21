ff955a08df031889f505b71136931f1c892f9b83
Tibor Ilić <tilic5@gmail.com>
2021-08-21 23:06:42 +0200

    ravn battle wip

--- script/battle/quest_battles/player/mission_ravnsvake/battle_script.lua
+++ script/battle/quest_battles/player/mission_ravnsvake/battle_script.lua
@@ -39,7 +39,21 @@ battle_ravnsvake_army_enemy = gb:get_army(gb:get_non_player_alliance_num(),"batt
 -- [320.78317260742, 113.35470581055, 336.00955200195]
 battle_ravnsvake_army_enemy:use_army_special_ability_on_message("drop_rocks", "wh_main_spell_rock_block_big", v(320.7, 113.3, 336.0), d_to_r(210));
 
+battle_ravnsvake_army_enemy:use_army_special_ability_on_message(
+	"all_units_transported_start_holes",
+	"wh2_main_spell_rat_hole_permanent",
+	v(270.7, 111.3, 306.0),
+	d_to_r(210)
+);
+battle_ravnsvake_army_enemy:use_army_special_ability_on_message(
+	"all_units_transported_start_holes",
+	"wh2_main_spell_rat_hole_permanent",
+	v(225.7, 111.3, 310.0),
+	d_to_r(210)
+);
 
+local has_to_blow_horn = false
+local next_hole_spawn_t = nil
 -------------------------------------------------------------------------------------------------
 ----------------------------------------- ARMY TELEPORT -----------------------------------------
 -------------------------------------------------------------------------------------------------
@@ -65,11 +79,11 @@ battle_ravnsvake_army_enemy:use_army_special_ability_on_message("drop_rocks", "w
 gb:message_on_time_offset("Wait_5_sec",30000);
 
 ga_battle_ravnsvake_army_enemy_reinf_ogres = gb:get_army(gb:get_non_player_alliance_num(),"battle_ravnsvake_army_enemy_reinf_ogres"); -- Reinforcement enemy army
-ga_battle_ravnsvake_army_enemy_reinf_ogres:reinforce_on_message("Wait_5_sec",5000);
+-- ga_battle_ravnsvake_army_enemy_reinf_ogres:reinforce_on_message("Wait_5_sec",5000);
 
---ga_battle_ravnsvake_army_enemy_reinf_ogres:deploy_at_random_intervals_on_message("Wait_5_sec",1,1,1000,1000); -- enemy reinforcements
---battle_ravnsvake_army_enemy_clan_rats = gb:get_army(gb:get_non_player_alliance_num(),"battle_ravnsvake_army_enemy_clan_rats"); -- Reinforcement enemy army
---battle_ravnsvake_army_enemy_clan_rats:deploy_at_random_intervals_on_message("Wait_5_sec",1,1,30000,30000); -- enemy reinforcements
+ga_battle_ravnsvake_army_enemy_reinf_ogres:deploy_at_random_intervals_on_message("Wait_5_sec",1,1,12000,12000); -- enemy reinforcements
+battle_ravnsvake_army_enemy_clan_rats = gb:get_army(gb:get_non_player_alliance_num(),"battle_ravnsvake_army_enemy_clan_rats"); -- Reinforcement enemy army
+battle_ravnsvake_army_enemy_clan_rats:deploy_at_random_intervals_on_message("Wait_5_sec",1,1,8000,8000); -- enemy reinforcements
 
 
 -------------------------------------------------------------------------------------------------
@@ -95,7 +109,7 @@ gb:queue_help_on_message("battle_started", "mission_ravnsvake_intro_msg", 8000, 
 	local function pj_ravn_blew_horn_on_special_ability(event)
 		local ability_key = event:get_string1()
 		out(ability_key)
-		if ability_key == "unit_active_horn_ice_valor" then
+		if has_to_blow_horn and ability_key == "unit_active_horn_ice_valor" then
  			gb.sm:trigger_message("blew_horn")
 		end
 	end
@@ -128,31 +142,62 @@ end
 gb:add_ping_icon_on_message("mission_ravnsvake_transport_army_msg", v(270.7, 93.3, 336.0), 11, 500, 5000);
 
 gb:complete_objective_on_message("all_units_transported", "mission_ravnsvake_transport_army_objective")
+gb:complete_objective_on_message("all_units_at_keep", "mission_ravnsvake_reach_keep_objective")
 gb:queue_help_on_message("all_units_transported", "mission_ravnsvake_to_keep_after_rocks_msg", 8000, 500)
 
 local gyro_dropoff_point = v(250.7, 73.3, 336.0)
+local keep_victory_point = v(137.7, 111.3, 540.0)
 
 local function update()
 	local alliances = bm:alliances()
 	local player_alliance = alliances:item(bm:local_alliance())
 	local player_army = player_alliance:armies():item(bm:local_army())
 
+	local current_battle_time = bm:time_elapsed_ms()
+
 	local are_all_units_transported = true
+	local are_all_units_at_keep = true
 
 	local player_units = player_army:units()
 	for i = 1, player_units:count() do
 		local current_unit = player_units:item(i);
 		local position = current_unit:position()
-		local distance = position:distance_xz(gyro_dropoff_point)
-		if not string.find(current_unit:type(), "gyro") and not current_unit:is_shattered() then
+		if not string.find(current_unit:type(), "gyro") and current_unit:type() ~= "wh_main_dwf_cha_lord_0" and not current_unit:is_shattered() then
+			local distance = position:distance_xz(gyro_dropoff_point)
+			local victory_distance = position:distance_xz(keep_victory_point)
 			are_all_units_transported = are_all_units_transported and distance <= 70
+			are_all_units_at_keep = are_all_units_at_keep and victory_distance <= 70
 		end
 		-- out(tostring(current_unit:type()))
 		-- out(tostring(distance))
 	end
 
 	if are_all_units_transported then
-		gb.sm:trigger_message("all_units_transported");
+		gb.sm:trigger_message("all_units_transported")
+		bm:callback(
+			function()
+				gb.sm:trigger_message("all_units_transported_start_holes")
+				next_hole_spawn_t = current_battle_time + 3000
+			end,
+			5000
+		)
+	end
+	if are_all_units_at_keep then
+		gb.sm:trigger_message("all_units_at_keep")
+	end
+
+	if next_hole_spawn_t and next_hole_spawn_t > current_battle_time then
+		next_hole_spawn_t = current_battle_time + 4000
+		local ability_that_spawns = "army_ability_spawn_clan_rats"
+		if bm:random_number(2) == 1 then
+			ability_that_spawns = "army_ability_spawn_ogres"
+		end
+		if bm:random_number(2) == 1 then
+			battle_ravnsvake_army_enemy:use_army_special_ability_on_message(tostring(current_battle_time), ability_that_spawns, v(270.7, 111.3, 306.0), d_to_r(210))
+		else
+			battle_ravnsvake_army_enemy:use_army_special_ability_on_message(tostring(current_battle_time), ability_that_spawns, v(225.7, 111.3, 310.0), d_to_r(210))
+		end
+		gb.sm:trigger_message(tostring(current_battle_time))
 	end
 end
 
@@ -175,6 +220,19 @@ bm:callback(function()
 	update()
 	real_timer.register_singleshot("rlk_ravnsvake_update_real_time_trigger", 50)
 end, 100, "rlk_ravnsvake_delay_update_cb")
+
+gb:add_listener(
+	"drop_rocks",
+	function()
+		bm:callback(
+			function()
+				has_to_blow_horn = true
+			end,
+			6500
+		)
+	end,
+	true
+)
 
 gb:add_listener(
 	"blew_horn",
@@ -200,6 +258,18 @@ gb:add_listener(
 	end,
 	false
 )
+
+local alliances = bm:alliances()
+local player_alliance = alliances:item(bm:local_alliance())
+local player_army = player_alliance:armies():item(bm:local_army())
+local player_units = player_army:units()
+
+for i = 1, player_units:count() do
+    local current_unit = player_units:item(i);
+    if current_unit then
+        current_unit:set_stat_attribute("wallbreaker", true)
+    end
+end
 -------------------------------------------------------------------------------------------------
 --------------------------------------------- HINTS/MESSAGES ---------------------------------------------
 -------------------------------------------------------------------------------------------------
